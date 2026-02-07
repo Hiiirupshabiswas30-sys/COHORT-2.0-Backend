9d347296f55e81bf5d9be96bd2984d341546f5b6
Rupsha <rupshabiswas30@gamil.com>
2026-02-07 16:29:06 +0530

    feat: Initialize backend with Express, MongoDB, and note management API
    - Added package.json with dependencies: dotenv, express, mongoose
    - Created server.js to start the Express server and connect to MongoDB
    - Implemented app.js for note management with CRUD operations
    - Established database connection in database.js
    - Defined note model in note.model.js

---
+++ day-9/Backend/src/config/database.js
@@ -0,0 +1,13 @@
+
+const mongoose = require("mongoose")
+
+function connectToDB(){
+    mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+
+        console.log("Connect to DB");
+        
+    })
+}
+
+module.exports = connectToDB
